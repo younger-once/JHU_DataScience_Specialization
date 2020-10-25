844c4d89eeb94f678e50ac24aeeef0985494ed79
younger-once <agrzasko@yahoo.com>
2020-10-24 19:04:17 -0500

    first commit

---
+++ Course3_GettingCleaningData/README.md
@@ -0,0 +1,79 @@
+# Getting and Cleaning Data Project
+
+## Overview  
+This repo contains an R script that downloads, wrangles, and summarizes wearable sensor data.  The script outputs a csv file in tidy data format.  
+ 
+## Raw Data    
+
+The data set is based on sensor signals extracted from Samsung Galaxy S II smartphones worn by 30 subjects at waist level.  Subjects were recorded while performing various functions such as standing, sitting, or walking.  
+ 
+ A full description of the data set can be found [here]
+(http://archive.ics.uci.edu/ml/datasets/[Human+Activity+Recognition+Using+Smartphones).
+  
+Raw data files used in this analysis were downloaded [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).  
+  
+## Repo Files 
+This repo contains the following primary data files:  
+  
+* `run_analysis.r` - R script to process raw data and output tidy dataset. 
+* `tidy_summary_data.csv` - file output (in tidy format)
+*  `codebook.md` - data dictionary with additional details on data transformations
+
+## Data Processing Steps
+
+The `run_analysis.r` script performs the following tasks:  
+  
+ 1. Downloads and unzips original data files
+ <br>
+ 
+ 2. Reads activity labels file
+ 3. Reads and processes feature names file
+ 	-  Retains features with mean and standard deviation measurements (i.e. features with terms "mean()" or "std()")
+ 	-  Modifies feature names to improve readability
+ 4. Reads and processes training data
+	- Assigns column names based on modified feature descriptions
+	- Joins in activity descriptions from activity label file
+	- Reads in training subject identifier file and combines with training data
+5. Reads and processes test data (with substeps similar to #4)
+6. Combines training and test data sets
+7. Transforms data to long format
+8. Breaks out feature names into individual components
+9. Creates a new data frame that summarizes average measurements for each of the following combinations:
+	- subject
+	- activity
+	- feature combinations
+	- measurement type (mean or standard deviation)
+- Outputs summary data frame as csv file, `tidy_summary_data.csv`
+
+## Tidy Data Commentary
+According to the [R for Data Science](https://r4ds.had.co.nz/tidy-data.html), tidy data is defined as follows: 
+
+-  Each variable is assigned to a unique column
+-  Every observation is organized into a single row
+-  Each value is organized in its own cell
+
+Satisfying any two of these rules implies that the remaining rule is also met. 
+ 
+For this assignment, each of the original feature names was interepreted to represent a composite description of multiple variables.  To get the data in tidy format, the feature names were first separated into individual columns to represent unique component variables--see the code book for details:  
+
+- dimension
+- signal
+- instrument
+- jerk
+- measureType
+- directionMag   
+
+The variable `measureType` has two distinct values:  "mean" and "std". These values were interpreted as describing two separate measure variables.  Therefore, the data set was transposed wide to produce two unique measure variable columns for the mean and standard deviation variables, respectively.        
+
+Each row of the summary data set contains average mean and standard deviation measurements for each subject, activity, and combination of feature variables.  This grouping is consistent with the observational units suggested by the assignment instructions: create a "tidy data set with the average of each variable for each activity and each subject".
+
+ 	 
+ 
+  
+
+ 
+ 
+
+
+ 
+
---
+++ Course3_GettingCleaningData/run_analysis.R
@@ -0,0 +1,79 @@
+# load relevant libraries
+library(dplyr)
+library(tidyr)
+
+# download and unzip files
+myurl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+download.file(myurl, "Dataset.zip"); unzip("Dataset.zip")
+
+# update working directory
+setwd("./UCI HAR Dataset")
+
+# read in activity label numbers and descriptions
+activity_labels <- read.table('activity_labels.txt',col.names=c("activityNum","activityDesc"))
+
+# read in feature names
+X_feature_names <- read.table('features.txt', col.names=c('featureNum','featureName'))
+
+# limit feature names to include "mean()" and "std()" only 
+# -- note: assuming "meanFreq()" and any other columns with "Mean" are not required for analysis
+# -- make feature names more readable (e.g. replace "t" with "Time", "f" with "Freq")
+X_feature_names <- X_feature_names[grep("mean\\(\\)|std\\(\\)",X_feature_names$featureName),] %>% 
+  mutate(featureName = sub("(?=Body|Gravity)","-",featureName, perl=T),
+         featureName = sub("(?=Acc|Gyro)","-",featureName,perl=T), 
+         featureName = ifelse(grepl("Mag",featureName),
+                              sub("\\(\\)","-Mag",featureName),featureName),
+         featureName = ifelse(grepl("Jerk",featureName),
+                              sub("Jerk","-Y",featureName),sub("(?<=Acc|Gyro)","-N",featureName, perl=T)),
+         featureName = sub("Mag","",featureName),
+         featureName = gsub("^t", "Time",featureName),
+         featureName = gsub("^f","Freq",featureName),
+         featureName = gsub("\\(\\)","",featureName)
+         )
+         
+# read training features data - limited to mean() and std() columns
+X_train <- read.table("train/X_train.txt")[, X_feature_names$featureNum]
+names(X_train) <- X_feature_names$featureName
+
+# read training activities data - using join to bring in activity descriptions 
+y_train <- read.table("train/y_train.txt", col.names="activityNum") %>% 
+  inner_join(activity_labels, by="activityNum")
+
+# read in training data subject number
+subject_train <- read.table("train/subject_train.txt",col.names="subject")
+
+# create training data set by combining multiple variables
+train <- cbind("dataset"="train",subject_train,"activity"=y_train$activityDesc,X_train)
+
+# read test features data - limited to mean() and std() columns
+X_test <- read.table("test/X_test.txt")[, X_feature_names$featureNum]
+names(X_test) <- X_feature_names$featureName
+
+# read test activities data - using join to bring in  activity descriptions 
+y_test <- read.table("test/y_test.txt", col.names="activityNum") %>% 
+  inner_join(activity_labels, by="activityNum")
+  
+# read in test data subject number
+subject_test <- read.table("test/subject_test.txt",col.names="subject")
+
+# create training data set by combining multiple variables
+test <- cbind("dataset"="test",subject_test,"activity"=y_test$activityDesc,X_test)
+
+# combine training and test data sets together into one data frame
+df <- rbind(train,test)
+
+# create long data frame; create separate variables for each feature component
+df <- df %>% gather("var","val",-dataset,-subject,-activity) %>%
+  separate(var,into=c("domain","signal","instrument","jerk","measureType","directionMag"))
+
+# create independent data frame that summarizes average measurement value for each 
+# -- subject, activity, feature combination, and measurement type
+# -- make data set "wider" by including both mean and standard deviation for each subject, activity,
+# -- and feature combination
+df_summary <- df %>% group_by(subject,activity,domain,signal,instrument,jerk,measureType,directionMag) %>%
+  summarise(meanVal=mean(val)) %>% spread(measureType,meanVal)
+
+# save summary data frame as csv file
+write.csv(df_summary,"../../tidy_summary_data.csv",row.names = F)
+
+
